8be839e2b3957501a56f5227c080d04b21c5e951
bulatzi <bulatzi@gmail.com>
2024-08-30 21:35:52 -0500

    Updates public asset filepaths to work locally and hosted on github

---
+++ assets/ResourcesPage.0e2ea760.js
@@ -0,0 +1,1 @@
+import{t as n,V as a}from"./index.3b9d53a4.js";var s=(e,t)=>{const r=e.__vccOpts||e;for(const[c,o]of t)r[c]=o;return r};const _={};function f(e,t){return n(),a("div",null,"test;")}var u=s(_,[["render",f]]);export{u as default};
--- assets/ResourcesPage.1e90638d.js
+++
@@ -1,1 +0,0 @@
-import{t as n,V as a}from"./index.af5fdb46.js";var s=(e,t)=>{const r=e.__vccOpts||e;for(const[c,o]of t)r[c]=o;return r};const _={};function f(e,t){return n(),a("div",null,"test;")}var u=s(_,[["render",f]]);export{u as default};
